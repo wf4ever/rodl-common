13d1abbee88ffd56682ed0d738089d99379c04c1
Piotr Hołubowicz <piotrhol@man.poznan.pl>
2012-10-19 10:30:48 +0200

    Removed the checkstyle file

--- src/main/java/pl/psnc/dl/wf4ever/vocabulary/ORE.java
+++ src/main/java/pl/psnc/dl/wf4ever/vocabulary/ORE.java
@@ -6,17 +6,18 @@ import com.hp.hpl.jena.ontology.OntModelSpec;
 import com.hp.hpl.jena.rdf.model.ModelFactory;
 import com.hp.hpl.jena.rdf.model.Property;
 
-
 public class ORE {
-    
+
     public static final String NAMESPACE = "http://www.openarchives.org/ore/terms/";
-    public static OntModel ontModel = ModelFactory.createOntologyModel(OntModelSpec.OWL_MEM,ModelFactory.createDefaultModel().read(NAMESPACE));
-    
+    public static OntModel ontModel = ModelFactory.createOntologyModel(OntModelSpec.OWL_MEM, ModelFactory
+            .createDefaultModel().read(NAMESPACE));
+
     public static final OntClass Proxy = ontModel.getOntClass(NAMESPACE + "Proxy");
     public static final Property describes = ontModel.getProperty(NAMESPACE + "describes");
     public static final Property isDescribedBy = ontModel.getProperty(NAMESPACE + "isDescribedBy");
     public static final Property aggregates = ontModel.getProperty(NAMESPACE + "aggregates");
     public static final Property proxyIn = ontModel.getProperty(NAMESPACE + "proxyIn");
     public static final Property proxyFor = ontModel.getProperty(NAMESPACE + "proxyFor");
+    public static final OntClass Aggregation = ontModel.getOntClass(NAMESPACE + "Aggregation");
 
 }
