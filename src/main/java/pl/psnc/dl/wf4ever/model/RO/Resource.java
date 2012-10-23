689d99efc01cbd6b5246f6111ca3e35f20b2f874
pejot <pejot@pejot-pcss.(none)>
2012-10-23 17:43:50 +0200

    fixing checkstyle

--- src/main/java/pl/psnc/dl/wf4ever/model/RO/Resource.java
+++ src/main/java/pl/psnc/dl/wf4ever/model/RO/Resource.java
@@ -2,11 +2,24 @@ package pl.psnc.dl.wf4ever.model.RO;
 
 import java.net.URI;
 
+/**
+ * Simple Resource model.
+ * 
+ * @author pejot
+ * 
+ */
 public class Resource {
 
+    /** Resource uri. */
     protected URI uri;
 
 
+    /**
+     * Contructor.
+     * 
+     * @param uri
+     *            reosurce uri
+     */
     public Resource(URI uri) {
         this.uri = uri;
     }
--- src/main/java/pl/psnc/dl/wf4ever/vocabulary/AO.java
+++ src/main/java/pl/psnc/dl/wf4ever/vocabulary/AO.java
@@ -4,10 +4,9 @@ import com.hp.hpl.jena.ontology.OntModel;
 import com.hp.hpl.jena.rdf.model.ModelFactory;
 import com.hp.hpl.jena.rdf.model.Property;
 
-
 public class AO {
 
-    public static  String NAMESPACE = "http://purl.org/ao/";
+    public static String NAMESPACE = "http://purl.org/ao/";
     public static OntModel ontModel = ModelFactory.createOntologyModel();
     public static Property body = ontModel.getProperty(NAMESPACE + "body");
     public static Property annotatesResource = ontModel.getProperty(NAMESPACE + "annotatesResource");
